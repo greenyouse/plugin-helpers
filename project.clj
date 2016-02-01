e90c7f990212519a595ec5b340534c46ea7f1386
greenyouse <greenyouse@hotmail.com>
2016-01-31 21:11:38 -0600

    Bump version to 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.greenyouse/plugin-helpers "0.1.1-SNAPSHOT"
+(defproject com.greenyouse/plugin-helpers "0.1.2"
   :description "Helper fns for leiningen plugins"
   :url "http://github.com/greenyouse/plugin-helpers"
   :license {:name "Eclipse Public License",
