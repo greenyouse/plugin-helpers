bd1ffd3f36276744188574404bce9c0b77026524
greenyouse <greenyouse@hotmail.com>
2016-02-08 09:52:34 -0600

    Add codox docs + travis CI

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
-(defproject com.greenyouse/plugin-helpers "0.1.4"
+(defproject com.greenyouse/plugin-helpers "0.1.5-SNAPSHOT"
   :description "Helper fns for leiningen plugins"
   :url "http://github.com/greenyouse/plugin-helpers"
   :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [leiningen-core "2.5.3"]
-                 [rewrite-clj "0.4.12"]])
+                 [rewrite-clj "0.4.12"]]
+
+  :profiles {:dev {:plugins [[lein-codox "0.9.1"]]}}
+
+  :codox {:source-uri "https://github.com/greenyouse/plugin-helpers/blob/master/{filepath}#L{line}"
+          :include [plugin-helpers.core]})
